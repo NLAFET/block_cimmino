98186a8e29bb2ce16940acd265a04750fa83743e
dwi <dwi@egrounds.org>
2012-09-10 17:32:42 +0200

    Factorization ready

--- src/abcd.cpp
+++ src/abcd.cpp
@@ -10,27 +10,35 @@ int abcd::bc(int job)
     switch(job) {
 
     case -1:
-        if(world.rank() == 0)
-            abcd::init();
+        if(world.rank() == 0){
+            abcd::initialize();
+        }
         world.barrier();
         break;
 
     case 1:
         if(world.rank() == 0) {
             abcd::preprocess();
-            abcd::partition();
-            abcd::frame_analysis();
+            abcd::partitionMatrix();
+            abcd::analyseFrame();
         }
         world.barrier();
         break;
 
     case 2:
         // Create the group of CG instances
-        abcd::inter_group_mapping();
+        abcd::createInterComm();
 
         if(instance_type == 0) {
-            abcd::distribute_parts();
+            abcd::distributePartitions();
         }
+        abcd::initializeCimmino();
+        if(instance_type == 0)
+            cout << "[+] Launching MUMPS analysis" << endl;
+        abcd::analyseAugmentedSystems();
+        if(instance_type == 0)
+            cout << "[+] Launching MUMPS factorization" << endl;
+        abcd::factorizeAugmentedSystems();
         break;
 
     case 3:
@@ -44,11 +52,11 @@ int abcd::bc(int job)
 }
 
 /// Creates and intializes the matrix object
-void abcd::init()
+void abcd::initialize()
 {
     typedef Eigen::Triplet<double> T;
     std::vector<T> elements;
-    
+
     // Check that the matrix data is present
     if(irn == 0 ||
             jcn == 0 ||
@@ -62,7 +70,8 @@ void abcd::init()
     if(sym) {
         for(unsigned k = 0; k < nz; ++k) {
             elements.push_back(T(irn[k] - 1, jcn[k] - 1, val[k]));
-            elements.push_back(T(jcn[k] - 1, irn[k] - 1, val[k]));
+            if(irn[k] != jcn[k])
+                elements.push_back(T(jcn[k] - 1, irn[k] - 1, val[k]));
         }
     } else {
         for(unsigned k = 0; k < nz; ++k) {
@@ -86,3 +95,4 @@ abcd::abcd()
 abcd::~abcd()
 {
 }
+
