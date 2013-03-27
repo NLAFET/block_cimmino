76722f06afe8e8f7711aa8fadc617eabe73fe797
Mohamed Zenadi <mzenadi@enseeiht.fr>
2013-03-27 10:48:09 +0100

    cleaning some memory

--- src/structure.cpp
+++ src/structure.cpp
@@ -160,7 +160,8 @@ void abcd::partitionMatrix()
             }
 
             cout << "    Finished Partitioning, time : " << MPI_Wtime() - t << endl;
-            delete[] ir, jc, val, partvec, partweights, cwghts, pins, xpins;
+            delete[] ir, jc, val, partvec, partweights, cwghts, pins, xpins, nwghts;
+            PaToH_Free();
             break;
     }
 
