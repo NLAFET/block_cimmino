3c8a09a8a30022ad110866e5abe025f891f8ddab
Mohamed Zenadi <mzenadi@enseeiht.fr>
2013-11-04 13:59:11 +0100

    Implemented a sample compression with test

--- test/src/test.cpp
+++ test/src/test.cpp
@@ -1,64 +1,52 @@
 #include "abcd.h"
-#include <iostream>
-#include <cstdio>
+#include "mat_utils.h"
+#include "gtest/gtest.h"
+#include "gmock/gmock.h"
+using ::testing::AtLeast;
+using ::testing::Return;
+using ::testing::Eq;
 
-#define BOOST_TEST_DYN_LINK
-#define BOOST_TEST_MODULE "Test_abcd Unit Tests"
-
-#include <boost/test/unit_test.hpp>
-
-using namespace std;
-
-BOOST_AUTO_TEST_CASE(my_test)
-{
-    BOOST_REQUIRE(true);
-
-    mpi::environment env;
-    mpi::communicator world;
-    abcd obj;
-
-    cout << "Hey, I'm process " << world.rank() << " of " << world.size() << endl;
-
-    // This should be done only by the master
-    if (world.rank() == 0) {
-        FILE *f;
-        MM_typecode mat_code;
-
-        // read the file and its content
-        f = fopen("/home/knuthy/stash/gre_1107/gre_1107.mtx", "r");
-        //     f = fopen("/home/knuthy/stash/s3_sym.mtx", "r");
-        //     f = fopen("/home/knuthy/stash/Huhs_DNA_CC.mtx", "r");
-        //     f = fopen("/home/knuthy/stash/1970", "r");
-        mm_read_banner(f, &mat_code);
-        mm_read_mtx_crd_size(f, (int *)&obj.m, (int *)&obj.n, (int *)&obj.nz);
-
-        if (mm_is_symmetric(mat_code))
-            obj.sym = true;
-        else
-            obj.sym = false;
-
-        // allocate the arrays
-        obj.irn = new int[obj.nz];
-        obj.jcn = new int[obj.nz];
-        obj.val = new double[obj.nz];
-
-        obj.start_index = 1;
-        obj.icntl[9] = 2;
-
-        obj.nbparts = 5;
-        obj.partitioning_type = 2;
-
-        mm_read_mtx_crd_data(f, obj.m, obj.n, obj.nz, obj.irn, obj.jcn, obj.val,
-                             mat_code);
-
-        cout << "Matrix information : ";
-        cout << "m = " << obj.m << "; n = " << obj.n << "; nz = " << obj.nz << endl;
-
-        fclose(f);
-
-        BOOST_CHECK(obj.bc(-1) == 0);
-        BOOST_CHECK(obj.bc(1) == 0);
-        BOOST_CHECK(obj.bc(2) == 0);
-    }
+TEST (getColumnIndex, MatrixWithSomeEmptyCols) { 
+    int sz = 10;
+    int col_ptr[11] = {0, 2, 2, 3, 3, 4, 5, 5, 7, 8, 9};
+    std::vector<int> ci;
+    ci.reserve(sz);
     
+    int j = 0;
+    for(int i = 1; i <= sz; i++) {
+        if(col_ptr[i] != col_ptr[i - 1]) ci.push_back(j);
+        j++;
+    }
+    // ci should be equal to {0, 2, 4, 5, 7, 8, 9}!
+    EXPECT_THAT(getColumnIndex(col_ptr, sz),Eq(ci));
+}
+
+TEST (getColumnIndex, MatrixWithNoEmptyCols) { 
+    int sz = 10;
+    int col_ptr[11] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+    std::vector<int> ci;
+    ci.reserve(sz);
+    
+    int j = 0;
+    for(int i = 1; i <= sz; i++) {
+        if(col_ptr[i] != col_ptr[i - 1]) ci.push_back(j);
+        j++;
+    }
+    // ci should be equal to {0,1,2,3,4,5,6,7,8,9}!
+    EXPECT_THAT(getColumnIndex(col_ptr, sz),Eq(ci));
+}
+
+TEST (getColumnIndex, MatrixWithOnlyEmptyCols) { 
+    int sz = 10;
+    int col_ptr[11] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+    std::vector<int> ci;
+    ci.reserve(sz);
+    
+    int j = 0;
+    for(int i = 1; i <= sz; i++) {
+        if(col_ptr[i] != col_ptr[i - 1]) ci.push_back(j);
+        j++;
+    }
+    // ci should be equal to {}!
+    EXPECT_THAT(getColumnIndex(col_ptr, sz),Eq(ci));
 }
