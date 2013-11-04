ff524635f5e3515437b8a376eeb65567d9bca518
Knuthy <knuthy@gmail.com>
2013-11-05 00:02:26 +0100

    checked and implemented a merge function for multiple vectors

--- src/utils/compress_columns.cpp
+++ src/utils/compress_columns.cpp
@@ -1,4 +1,6 @@
 #include "vect_utils.h"
+#include <climits>
+#include <iostream>
 
 //! \brief Returns the index of non-null columns
 //! @param[in] col_ptr Compressed column array
@@ -33,13 +35,37 @@ std::vector<int> mergeSortedVectors(std::vector<std::vector<int> > &vectors)
     std::vector<int> merge;
     if(vectors.size() == 1) return vectors[0];
     
-    std::vector<std::vector<int>::iterator> its(vectors.size());
-    std::vector<std::vector<int>::iterator> eds(vectors.size());
+    std::map<int,std::vector<int>::iterator> its;
+    std::map<int,std::vector<int>::iterator> eds;
     for(int i = 0; i < vectors.size(); i++)
     {
         its[i] = vectors[i].begin();
         eds[i] = vectors[i].end();
     }
-    
+
+    int min, min_index, to_delete = -1;
+    while(its.size() != 0)
+    {
+        min = INT_MAX;
+        to_delete = -1;
+        min_index = -1;
+        for(std::map<int,std::vector<int>::iterator>::iterator it = its.begin();
+                it != its.end(); it++)
+        {
+            if(*it->second < min){
+                min_index = it->first;
+                min = *it->second;
+
+                if(it->second + 1 == eds[it->first])
+                {
+                    to_delete = it->first;
+                }
+            } else if(*it->second == min) it->second++;
+        }
+
+        merge.push_back(*its[min_index]);
+        its[min_index]++;
+        if(to_delete != -1) its.erase(to_delete);
+    }
     return merge;
 }
