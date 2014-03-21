308d63d93c33fad0816439005d2fa9b9eb93d425
Mohamed Zenadi <mzenadi@enseeiht.fr>
2014-03-21 19:27:36 +0100

    documentation

---
+++ doc/gen.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/sh
+
+
+while inotifywait -e modify usage.md; do
+    pandoc -s --mathjax -V geometry:margin=1in -f markdown+footnotes+definition_lists\
+        --highlight-style=tango\
+        -o abcd_usage.pdf usage.md
+    pandoc -s --mathjax -f markdown+footnotes+definition_lists\
+        --highlight-style=tango\
+        -o abcd_usage.html usage.md
+done
