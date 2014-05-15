bba39930941f0e742537f2ab307a1a4d01d2dedd
Ronan Guivarch <Ronan.Guivarch@enseeiht.fr>
2014-05-15 17:59:58 +0200

    premier exemple c

--- test/src/abcd_c.h
+++ test/src/abcd_c.h
@@ -1,13 +1,27 @@
 struct abcd_solver
 {
+  int m; ///< row number
+  int n; ///< column number
+  int nz; ///< number of nnz in the lower-triangular part
   int *icntl;
   int *dcntl;
+  int sym;
+  
+  char *write_problem;
+
+  int *irn;
+  int *jcn;
+  double *val;
+
+  double *rhs;
 };
 
 #ifndef __cplusplus
 struct abcd_solver* new_solver();
+    void call_solver(struct abcd_solver *solver, int job_id);
 #else
 extern "C" {
     extern struct abcd_solver* new_solver();
+    extern void call_solver(struct abcd_solver *solver, int job_id);
 }
 #endif
--- test/src/c_interface.cc
+++ test/src/c_interface.cc
@@ -1,15 +1,49 @@
 #include "abcd.h"
 #include "abcd_c.h"
+#include <string>
 
 abcd *obj;
 
 #include <iostream>
 using namespace std;
+/** description breve
+ *
+ * detailleeeee
+ * @return a new solver
+ */
 struct abcd_solver* new_solver()
 {
-  struct abcd_solver *solver = (struct abcd_solver *)malloc(sizeof(*solver));
+  struct abcd_solver *solver = (struct abcd_solver*) malloc(sizeof(*solver));
+
   obj = new abcd();
-  cout << obj->icntl[Controls::nbparts] << '\t' << Controls::nbparts << endl;
-  solver->icntl = &obj->icntl[0];
+  //solver -> icntl = &obj -> icntl[0];
   return solver;
 }
+void call_solver(struct abcd_solver* solver, int job_id) 
+{
+  // C -> CPP
+  //obj->write_problem = string(solver->write_problem);
+  obj -> m = solver -> m;
+  obj -> n = solver -> n;
+  obj -> nz = solver -> nz;
+  obj -> sym = (solver -> sym != 0);
+  obj -> irn = solver -> irn;
+  obj -> jcn = solver -> jcn;
+  obj -> val = solver -> val;
+  obj -> rhs = solver -> rhs;
+  cout << obj-> sym << endl;
+  cout << obj-> m << endl;
+  cout << obj-> n << endl;
+  cout << obj-> nz << endl;
+  for (size_t i = 0; i < obj -> nz; i++) {
+    cout << obj-> irn[i] << "  " << obj-> jcn[i] << "  " << obj -> val[i]<< endl;
+  }  
+
+  // TRAVAIL
+  obj -> operator()(job_id);
+  //cout << obj->write_problem << endl;
+  //obj -> write_problem =  string("aaa");
+  // CPP -> C
+  //solver -> write_problem = (char *)(obj -> write_problem).c_str();
+}
+
