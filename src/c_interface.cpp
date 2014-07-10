dee2666a572d2bf674035e3f714efbfce4747775
Mohamed Zenadi <mzenadi@enseeiht.fr>
2014-07-10 16:03:25 +0200

    some cleanup of the C interface

--- src/c_interface.cpp
+++ src/c_interface.cpp
@@ -14,40 +14,40 @@ using namespace std;
  */
 struct abcd_solver* new_solver()
 {
-  struct abcd_solver *solver = (struct abcd_solver*) malloc(sizeof(*solver));
+    struct abcd_solver *solver = (struct abcd_solver*) malloc(sizeof(*solver));
 
-  obj = new abcd();
-  //solver -> icntl = &obj -> icntl[0];
-  return solver;
+    obj = new abcd();
+    //solver -> icntl = &obj -> icntl[0];
+    return solver;
 }
 void call_solver(struct abcd_solver* solver, int job_id) 
 {
-  // C -> CPP
-  //obj->write_problem = string(solver->write_problem);
-  obj -> m = solver -> m;
-  obj -> n = solver -> n;
-  obj -> nz = solver -> nz;
-  obj -> sym = (solver -> sym != 0);
-  obj -> irn = solver -> irn;
-  obj -> jcn = solver -> jcn;
-  obj -> val = solver -> val;
-  obj -> rhs = solver -> rhs;
-  cout << obj-> sym << endl;
-  cout << obj-> m << endl;
-  cout << obj-> n << endl;
-  cout << obj-> nz << endl;
-  for (size_t i = 0; i < obj -> nz; i++) {
-    cout << obj-> irn[i] << "  " << obj-> jcn[i] << "  " << obj -> val[i]<< endl;
-  } 
-  for(size_t i = 0; i < obj -> icntl.size(); i++){
-    obj -> icntl[i] = solver -> icntl[i];
-  }
- obj -> icntl[Controls::verbose_level] = 2;
-  // TRAVAIL
-  obj -> operator()(job_id);
-  //cout << obj->write_problem << endl;
-  //obj -> write_problem =  string("aaa");
-  // CPP -> C
-  //solver -> write_problem = (char *)(obj -> write_problem).c_str();
+    // C -> CPP
+    obj->m   = solver->m;
+    obj->n   = solver->n;
+    obj->nz  = solver->nz;
+    obj->sym = (solver->sym != 0);
+    obj->irn = solver->irn;
+    obj->jcn = solver->jcn;
+    obj->val = solver->val;
+    obj->rhs = solver->rhs;
+    obj->write_problem = string(solver->write_problem);
+
+    for(size_t i = 0; i < obj -> icntl.size(); i++){
+        obj -> icntl[i] = solver -> icntl[i];
+    }
+
+    // call the solver
+    obj -> operator()(job_id);
+
+    // CPP -> C
+    //solver -> write_problem = (char *)(obj -> write_problem).c_str();
 }
 
+
+void free_solver(struct abcd_solver *solver)
+{
+    delete obj;
+
+    free(solver);
+}
