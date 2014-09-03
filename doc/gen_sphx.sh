b15b0ced5a0cb408446eb5de44e9a90dbb6e3781
Mohamed Zenadi <mzenadi@enseeiht.fr>
2014-09-03 13:28:39 +0200

    updated the doc generator

--- doc/gen_sphx.sh
+++ doc/gen_sphx.sh
@@ -5,26 +5,33 @@ export SB="/usr/bin/env sphinx-build"
 function do_job {
   $SB -b html -E -j2 source build/html
   $SB -b latex -E -j2 source build/latex
-  if [ "$1" == "pdf" ]
-      then
-        cd build/latex
-        sed -n '
-        1h
-        1!H
-        $ {
-          g
-          s/\\begin{fulllineitems}\n*\\end{fulllineitems}//g
-          p
-        }' ABCDSolver.tex > temp.tex
-        rm ABCDSolver.tex
-        mv temp.tex ABCDSolver.tex
-        make clean
-        make
-        cd -
-  fi
+  cd build/latex
+  sed -n '
+  1h
+  1!H
+  $ {
+    g
+    s/\\begin{fulllineitems}\n*\\end{fulllineitems}//g
+    p
+  }' ABCDSolver.tex > temp.tex
+  rm ABCDSolver.tex
+  mv temp.tex ABCDSolver.tex
+  make clean
+  echo "Building pdf file"
+  make &> /dev/null
+  cd -
 }
 
 do_job $1
+
+if [ "$1" == "update" ]
+      then
+        echo "Uploading to abcd.enseeiht.fr"
+        rsync -raz --progress --delete \
+          build/latex/ABCDSolver.pdf \
+          build/html/* \
+          mzenadi@abcd.enseeiht.fr:/var/www/abcd/
+fi
 
 # while inotifywait -e modify ./source; do
     # echo "has changed, working..."
